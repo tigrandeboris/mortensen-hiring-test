ef29acd765a7d78564828f0c47e7c3d788001769
Tigran <tigran.harutyunyan@hexad.de>
2022-03-16 17:04:42 +0100

    ADD mobile version

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,5 +4,18 @@
  */
 
 +(function () {
-  // Swiper
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
 })();
